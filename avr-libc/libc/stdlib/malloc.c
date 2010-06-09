Pitchumani/avr-libc

commit 387e36d13f20fd4c42f585970943628cf410c3df

bug #27235, malloc: Several things go wrong * libc/stdlib/malloc.c (malloc): Revert the patch from r2123, as it is no longer needed. * libc/stdlib/malloc.c (free): When freeing the topmost chunk of the heap, reduce __brkval again, returning the area from the heap entirely. * doc/api/malloc.dox: Mention that the heap is now reduced if possible. * tests/simulate/regression/bug-27242.c: Adapt testscript to match the new behaviour. * tests/simulate/stdlib/malloc-1.c: (Ditto.) * tests/simulate/stdlib/malloc-6.c: (Ditto.) * tests/simulate/stdlib/malloc-7.c: (Ditto.) * tests/simulate/stdlib/realloc-2.c: New file (test whether subsequent allocation does not extend the heap). * tests/simulate/regression/bug-28135.c: New file, another regression test (actual bugfix from bug #28135 is no longer needed)
git-svn-id: svn://svn.savannah.nongnu.org/avr-libc/trunk@2131 1084037a-409f-4a99-8160-1a4b3577d4c1

avr-libc/libc/stdlib/malloc.c
@@ -164,22 +164,7 @@ malloc(size_t len)
 	 * segment as dynamically requested from the operating system.
 	 * Since we don't have an operating system, just make sure
 	 * that we don't collide with the stack.
-	 * But first we check if there is a free chunk at the end of
-	 * the allocation area. Brkval is corrected downwards then,
-	 * so the free chunk gets added to the new chunk.
-	 * (fp2 still points to the last list entry from step 1)
 	 */
-	if (fp2 && (char*)fp2 + fp2->sz + sizeof(size_t) == __brkval) {
-    	__brkval = (char*)fp2;
-		for (fp1 = __flp, fp2 = 0;
-		     fp1->nx;
-	    	 fp2 = fp1, fp1 = fp1->nx)
-			{}
-		if (fp2)
-			fp2->nx = 0;
-		else
-			__flp = 0;
-	}
 	if (__brkval == 0)
 		__brkval = __malloc_heap_start;
 	cp = __malloc_heap_end;
@@ -225,10 +210,14 @@ free(void *p)
 
 	/*
 	 * Trivial case first: if there's no freelist yet, our entry
-	 * will be the only one on it.
+	 * will be the only one on it.  If this is the last entry, we
+	 * can reduce __brkval instead.
 	 */
 	if (__flp == 0) {
-		__flp = fpnew;
+		if ((char *)p + fpnew->sz == __brkval)
+			__brkval = cpnew;
+		else
+			__flp = fpnew;
 		return;
 	}
 
@@ -268,6 +257,22 @@ free(void *p)
 		/* lower junk adjacent, merge */
 		fp2->sz += fpnew->sz + sizeof(size_t);
 		fp2->nx = fpnew->nx;
+	}
+	/*
+	 * If there's a new topmost chunk, lower __brkval instead.
+	 */
+	while (fp2->nx != NULL) {
+		fp1 = fp2;
+		fp2 = fp2->nx;
+	}
+	cp2 = (char *)&(fp2->nx);
+	if (cp2 + fp2->sz == __brkval) {
+		if (fp1 == NULL)
+			/* Freelist is empty now. */
+			__flp = NULL;
+		else
+			fp1->nx = NULL;
+		__brkval = cp2 - sizeof(size_t);
 	}
 }
 

avr-libc/doc/api/malloc.dox
@@ -214,7 +214,8 @@ When calling free(), a new freelist entry will be prepared.  An
 attempt is then made to aggregate the new entry with possible adjacent
 entries, yielding a single larger entry available for further
 allocations.  That way, the potential for heap fragmentation is hopefully
-reduced.
+reduced.  When deallocating the topmost chunk of memory, the size of the
+heap is reduced.
 
 A call to realloc() first determines whether the operation is about to
 grow or shrink the current allocation.  When shrinking, the case is

avr-libc/tests/simulate/regression/bug-28135.c
@@ -36,66 +36,90 @@
 #include "../../libc/stdlib/stdlib_private.h"
 
 
-/* Test code from bug #27242 (and #25723) */
+/* Test code from bug # */
 int main(void)
 {
 	char *p, *p1;
 
-	p = malloc(16);
+	/* Step 1: test with just one free chunk */
+
+	p = malloc(20);
 	if (!p)
 		return 1;
 
-	/* releasing 6 bytes creates a new free chunk */
+	/* releasing 10 bytes returns them from the heap */
 	p1 = realloc(p, 10);
 	if (p != p1)
 		return 2;
 
-	/*
-	 * The patch for bug #25723 introduces a regression to realloc()
-	 * when called to shrink a memory chunk. We simply verify if
-	 * the new __brkval starts immediately after the last allocated
-	 * chunk.
-	 */
-	p = p1 + 10;
-	if (__brkval != p)
+	if (__flp)
 		return 3;
 
-	/* use the last free chunk */
-	p1 = malloc(4);
-	if (!p1)
+	/* should extend the free chunk */
+	p = malloc(15);
+	if (!p)
 		return 4;
 
 	/* should be empty */
 	if (__flp)
 		return 5;
 
-	p = malloc(10);
-	if (!p)
+	/* should be the following chunk */
+	p1 += 10 + sizeof(size_t);
+	if (p != p1)
 		return 6;
 
-	/* force creation of a new minimal free chunk (sz = 3), which
-           will be returned from heap then */
-	p1 = realloc(p, 5);
-	if (p != p1)
+	/* should point right after the last chunk */
+	p += 15;
+	if (p != __brkval)
 		return 7;
 
-	p = p1 + 5;
-	if (__brkval != p)
+	/* Step 2: Test with 2 free chunks. */
+
+	p = malloc(8);
+	if (!p)
 		return 8;
 
-	/* merge with the free chunk (incr = 4 > sz = 3) */
-	p = realloc(p1, 9);
-	if (p != p1)
+	p1 = malloc(9);
+	if (!p1)
 		return 9;
 
-	/* chunk size should be 9 */
-	p -= sizeof(size_t);
-	if (*(size_t *)p != 9)
+	free(p);
+
+	p = malloc(20);
+	if (!p)
 		return 10;
 
-	/* should be empty */
-	if (__flp)
+	if (!__flp)
 		return 11;
+
+	p1 = realloc(p, 10);
+	if (p1 != p)
+		return 12;
+
+	if (!__flp)
+		return 13;
+
+	if (__flp->nx)
+		return 14;
+
+	p = malloc(15);
+	if (!p)
+		return 15;
+
+	if (!__flp)
+		return 16;
+
+	if (__flp->nx)
+		return 17;
+
+	p1 += 10 + sizeof(size_t);
+	if (p != p1)
+		return 18;
+
+	p += 15;
+	if (p != __brkval)
+		return 19;
 
 	return 0;
 }

avr-libc/tests/simulate/stdlib/malloc-6.c
@@ -67,11 +67,9 @@ int main(void)
     }
 
     free(ptrs[5]);
-    /* There must be exactly one entry in freelist. */
-    if ((char *)(ptrs[5]) - 2 != (void *)__flp) return __LINE__;
-    if (__flp->sz != 10) return __LINE__;
-    if (__flp->nx != NULL) return __LINE__;
-    if ((char *)(ptrs[5]) + 10 != (void *)__brkval) return __LINE__;
+    /* Freelist must be still empty, and __brkval reduced. */
+    if (__flp != NULL) return __LINE__;
+    if ((char *)(ptrs[5]) - 2 != __brkval) return __LINE__;
 
     return 0;
 }

avr-libc/tests/simulate/stdlib/malloc-7.c
@@ -67,39 +67,32 @@ int main(void)
     }
 
     free(ptrs[5]);
-    /* There must be exactly one entry in freelist. */
-    if ((char *)(ptrs[5]) - 2 != (void *)__flp) return __LINE__;
-    if (__flp->sz != 10) return __LINE__;
-    if (__flp->nx != NULL) return __LINE__;
-    if ((char *)(ptrs[5]) + 10 != (void *)__brkval) return __LINE__;
+    /* Freelist must be still empty, and __brkval reduced. */
+    if (__flp != NULL) return __LINE__;
+    if ((char *)(ptrs[5]) - 2 != __brkval) return __LINE__;
 
     free(ptrs[4]);
-    /* Still one entry expected. */
-    if ((char *)(ptrs[4]) - 2 != (void *)__flp) return __LINE__;
-    if (__flp->sz != 22) return __LINE__;
-    if (__flp->nx != NULL) return __LINE__;
-    if ((char *)(ptrs[4]) + 22 != (void *)__brkval) return __LINE__;
+    /* Still no entry, and __brkval further down. */
+    if (__flp != NULL) return __LINE__;
+    if ((char *)(ptrs[4]) - 2 != __brkval) return __LINE__;
 
     struct __freelist *ofp = __flp;
 
     free(ptrs[1]);
-    /* Another entry added. */
+    /* One entry added. */
     if ((char *)(ptrs[1]) - 2 != (void *)__flp) return __LINE__;
     if (__flp->sz != 10) return __LINE__;
     if (__flp->nx != (void *)ofp) return __LINE__;
 
     free(ptrs[3]);
-    /* Second entry must have been extended. */
-    if ((char *)(ptrs[3]) - 2 != (void *)__flp->nx) return __LINE__;
-    if (__flp->nx->sz != 34) return __LINE__;
-    if (__flp->nx->nx != NULL) return __LINE__;
-    if ((char *)(ptrs[3]) + 34 != (void *)__brkval) return __LINE__;
+    /* __brkval lowered again. */
+    if (__flp->nx != NULL) return __LINE__;
+    if ((char *)(ptrs[3]) - 2 != __brkval) return __LINE__;
 
     free(ptrs[2]);
-    /* Both chunks must have been combined. */
-    if (__flp->sz != 58) return __LINE__;
+    /* ...and again. */
     if (__flp->nx != NULL) return __LINE__;
-    if ((char *)(ptrs[1]) + 58 != (void *)__brkval) return __LINE__;
+    if ((char *)(ptrs[1]) - 2 != __brkval) return __LINE__;
 
     return 0;
 }

avr-libc/tests/simulate/stdlib/realloc-2.c
@@ -29,8 +29,8 @@
 
 /* $Id$ */
 
-/* Allocate a bunch of things, and free them again.  At the end,
-   the freelist is supposed to consist of a single entry. */
+/* Like malloc-1.c, but after freeing everything, try to subsequently
+   allocate everything through a single pointer, and realloc(). */
 
 #ifndef	__AVR__
 
@@ -88,6 +88,8 @@ int main(void)
        match the expected TARGETVAL */
     if (__brkval - __malloc_heap_start != TARGETVAL) return __LINE__;
 
+    char *obrk = __brkval;
+
     for (i = 0; i < 8; i++)
     {
         free(ptrs[i]);
@@ -95,6 +97,21 @@ int main(void)
 
     /* after freeing everything, the freelist must be empty */
     if (__flp != NULL) return __LINE__;
+    if (__brkval != __malloc_heap_start) return __LINE__;
+
+    size_t s = sizes[0];
+    void *p = malloc(s);
+    if (p == NULL) return __LINE__;
+    for (i = 1; i < 8; i++)
+    {
+        s += sizes[i];
+        void *q = realloc(p, s);
+        if (q == NULL) return __LINE__;
+        if (q != p) return __LINE__;
+    }
+
+    /* Verify the allocation length did not increase. */
+    if (__brkval > obrk) return __LINE__;
 
     return 0;
 }

commit 0f3184215868dd7a7fe5e2403c6afae31228575f

Fix a bug introduced in r2131 that could cause the freelist to be discarded.
git-svn-id: svn://svn.savannah.nongnu.org/avr-libc/trunk@2149 1084037a-409f-4a99-8160-1a4b3577d4c1

avr-libc/libc/stdlib/malloc.c
@@ -254,17 +254,17 @@ free(void *p)
 	/*
 	 * If there's a new topmost chunk, lower __brkval instead.
 	 */
-	while (fp2->nx != NULL) {
-		fp1 = fp2;
-		fp2 = fp2->nx;
-	}
-	cp2 = (char *)&(fp2->nx);
-	if (cp2 + fp2->sz == __brkval) {
-		if (fp1 == NULL)
+	for (fp1 = __flp, fp2 = 0;
+	     fp1->nx != 0;
+	     fp2 = fp1, fp1 = fp1->nx)
+		/* advance to entry just before end of list */;
+	cp2 = (char *)&(fp1->nx);
+	if (cp2 + fp1->sz == __brkval) {
+		if (fp2 == NULL)
 			/* Freelist is empty now. */
 			__flp = NULL;
 		else
-			fp1->nx = NULL;
+			fp2->nx = NULL;
 		__brkval = cp2 - sizeof(size_t);
 	}
 }